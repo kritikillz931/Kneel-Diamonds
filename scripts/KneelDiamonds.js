927561dc9bb8e65c2e63c2aebc31a9870598226e
Justin Adams <justin@DESKTOP-GP5T8ES.localdomain>
2021-05-03 16:31:16 -0500

    Automatically displaying new orders

--- scripts/KneelDiamonds.js
+++ scripts/KneelDiamonds.js
@@ -2,10 +2,14 @@ import { Metals } from "./Metals.js";
 import { JewelryStyles } from "./JewelryStyles.js";
 import { DiamondSizes } from "./DiamondSizes.js";
 import { Orders } from "./Orders.js";
+import { addCustomOrder } from "./database.js";
 
 document.addEventListener(
     "click",
     (event) => {
+        if (event.target.id === "orderButton") {
+            addCustomOrder()
+        }
     }
 )
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,6 @@
 import { addCustomOrder, orderBuilder } from "./database.js"
 import { KneelDiamonds } from "./KneelDiamonds.js"
 
-document.addEventListener("stateChanged", event => {
-    console.log("State of data has changed. Regenerating HTML...")
-    renderAllHTML()
-})
 
 const mainContainer = document.querySelector("#container")
 
@@ -12,6 +8,11 @@ const renderAllHTML = () => {
     mainContainer.innerHTML = KneelDiamonds()
 }
 
-addCustomOrder(orderBuilder())
+
 renderAllHTML()
 
+
+document.addEventListener("stateChanged", event => {
+    console.log("State of data has changed. Regenerating HTML...")
+    renderAllHTML()
+})
